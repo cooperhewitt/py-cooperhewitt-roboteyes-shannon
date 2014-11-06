0233227961072167b2df09206336f54123b5a91b
copea <copea@si.edu>
2014-11-06 15:51:01 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cooperhewitt-roboteyes-shannon',
+      version='0.1',
+      description='',
+      author='Cooper Hewitt Smithsonian Design Museum',
+      url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon',
+      requires=[
+        # please write me
+        ],
+      packages=[
+        'cooperhewitt',
+        'cooperhewitt.roboteyes',
+        'cooperhewitt.roboteyes.shannon',
+        ]
+      scripts=[],
+      download_url='https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-shannon/releases/tag/v0.1',
+      license='BSD')
